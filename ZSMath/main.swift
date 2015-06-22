5338a1cf31b3c3d86e222a84f0c88024004b53c4
Zane Swafford <zane@zaneswafford.com>
2015-06-22 12:22:13 -0500

    Initial Commit

---
+++ ZSMath/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ZSMath
+//
+//  Created by Zane Swafford on 6/22/15.
+//  Copyright (c) 2015 Zane Swafford. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
